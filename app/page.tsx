a77a359b3dfabbe2d3f6179ab7330e67af6ccb5f
Prateek7071 <tiwariprateek123@gmail.com>
2025-11-07 07:47:04 +0530

    Created login path, button and components like , login buttom, form, and card wrapper.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,38 @@
-"use client";
+import { Lock, User, Mail, Eye, EyeOff } from 'lucide-react';
+import { Poppins } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { LoginButton } from '@/components/auth/login-button';
 
+const font = Poppins({
+  subsets:["latin"],
+  weight: ["600"]
+})
 
-import {Button} from "@/components/ui/button"
-import { useEffect } from "react";
 export default function Home() {
-  console.log(" ✔️ This will render on server aka your terminal OR on the browser(client) if used 'user client' ")
-  
-  useEffect(()=>{
-    console.log("Mounted")
-  },[])
   return (
-    <Button>Click me</Button>
+    <main className="flex h-full flex-col items-center justify-center 
+      bg-linear-to-b from-sky-400 to-blue-800">
+    
+        <div className="space-y-6 text-center">
+          <div className="flex items-center justify-center gap-4">
+          <Lock className='w-12 h-12 text-white' />
+          <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md",font.className)}>
+            Auth
+          </h1>
+          </div>
+          <p className="text-white text-lg">
+            A simple authentication serivce
+          </p>
+          <div>
+            <LoginButton>  
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+            </LoginButton>
+          </div>
+        </div>
+    </main>
  )
 }
 
