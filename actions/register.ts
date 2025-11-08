664852ed1cbfe45d55e355597f06b6ec8529455f
Prateek7071 <tiwariprateek123@gmail.com>
2025-11-08 22:44:42 +0530

    create user functionality achieved

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server";
 import * as z from "zod";
+import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register =async (values: z.infer<typeof RegisterSchema>) =>{
   const ValdidateFields = RegisterSchema.safeParse(values);
@@ -8,6 +11,25 @@ export const register =async (values: z.infer<typeof RegisterSchema>) =>{
   if(!ValdidateFields.success){
     return {error: "Invalid fields!"}
   }
+  
+  const {name, email, password } = ValdidateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10)
+  
+  const existingUser = await getUserByEmail(email);
+  
+  if(existingUser){ 
+    return { error: "Email already in use!" }
+  }
+  
+  await db.user.create({
+    data:{
+      name,
+      email,
+      password:hashedPassword,
+    }
+  })
+  
+  //Todo: Send verification token
   
   return {success: "Email sent!"}
 }
